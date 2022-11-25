5418f18fab991315d223d54a13f6878cede04a1b
DigitalCrafting <k.kasprzyk2@gmail.com>
2022-11-25 23:25:25 +0100

    [C1] Initial proxy support, no authorization

--- chapter-1/src/http.c
+++ chapter-1/src/http.c
@@ -8,6 +8,11 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
+#define MAX_GET_COMMAND 255
+#define BUFFER_SIZE 255
+#define HTTP_PORT 80
+
+
 /* ====================================== */
 
 /*
@@ -35,16 +40,76 @@ int parse_url(char *uri, char **host, char **path) {
 }
 
 /* ====================================== */
+/*
+ * Parse proxy parameters from schema: http://[username:password@]proxy-host:proxy-port
+ */
+int parse_proxy_param(char *proxy_spec, char **proxy_host, int *proxy_port, char **proxy_user, char **proxy_password) {
+    char *login_sep, *colon_sep, *trailer_sep;
 
-#define MAX_GET_COMMAND 255
+    /* Technically, user chould start the proxy spec with http://, but let's be forgiving */
+    if (!strncmp("http://", proxy_spec, 7)) {
+        proxy_spec += 7;
+    }
 
+    login_sep = strchr(proxy_spec, '@');
+
+    if (login_sep) {
+        colon_sep = strchr(proxy_spec, ':');
+        if (!colon_sep || (colon_sep > login_sep)) {
+            // Error - if username supplied then password must also be supplied
+            fprintf(stderr, "Expected password in '%s'\n", proxy_spec);
+            return 0;
+        }
+
+        *colon_sep = '\0';
+        *proxy_user = proxy_spec;
+        *login_sep = '\0';
+        *proxy_password = colon_sep + 1;
+        proxy_spec - login_sep + 1;
+    }
+
+    // Ignore a '/' added at the end sometimes
+    trailer_sep = strchr(proxy_spec, '/');
+    if (trailer_sep) {
+        *trailer_sep = '\0';
+    }
+
+    colon_sep = strchr(proxy_spec, ':');
+    if (colon_sep) {
+        // non-standard proxy port
+        *colon_sep = '\0';
+        *proxy_host = proxy_spec;
+        *proxy_port = atoi(colon_sep + 1);
+        if (*proxy_port == 0) {
+            /* 0 is not a valid port - this is an error whether it was mistyped or specified that way */
+            return 0;
+        }
+    } else {
+        *proxy_port = HTTP_PORT;
+        *proxy_host = proxy_spec;
+    }
+
+    return 1;
+}
+
+/* ====================================== */
 /*
  * Format and send an HTTP get command.
  */
-int http_get(int connection, const char *path, const char *host) {
+int http_get(int connection,
+             const char *path,
+             const char *host,
+             const char *proxy_host,
+             const char *proxy_user,
+             const char *proxy_password) {
     static char get_command[MAX_GET_COMMAND];
 
-    sprintf(get_command, "GET /%s HTTP/1.1\r\n", path);
+    if (proxy_host) {
+        sprintf(get_command, "GET http://%s/%s HTTP:/1.1\r\n", host, path);
+    } else {
+        sprintf(get_command, "GET /%s HTTP/1.1\r\n", path);
+    }
+
     if (send(connection, get_command, strlen(get_command), 0) == -1) {
         return -1;
     }
@@ -63,9 +128,6 @@ int http_get(int connection, const char *path, const char *host) {
 }
 
 /* ====================================== */
-
-#define BUFFER_SIZE 255
-
 /*
  * Receive all data available on a connection and dump it to stdout
  */
@@ -74,7 +136,7 @@ void display_result(int connection) {
 
     static char recv_buf[BUFFER_SIZE + 1];
 
-    while ((received = recv(connection,recv_buf, BUFFER_SIZE, 0)) > 0) {
+    while ((received = recv(connection, recv_buf, BUFFER_SIZE, 0)) > 0) {
         recv_buf[received] = '\0';
         printf("%s", recv_buf);
     }
@@ -83,38 +145,51 @@ void display_result(int connection) {
 }
 
 /* ====================================== */
-
-#define HTTP_PORT 80
-
 /*
  * Simple command-line HTTP client
  */
 int main(int argc, char *argv[]) {
     int client_connection;
+    char *proxy_host, *proxy_user, *proxy_password;
+    int proxy_port;
     char *host, *path;
     struct hostent *host_name;
     struct sockaddr_in host_address;
+    int ind;
 
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s: <URL>\n", argv[0]);
+        fprintf(stderr, "Usage: %s: [-p http://[username:password@]proxy-host:proxy-port] <URL>\n", argv[0]);
         return 1;
     }
 
-    if (parse_url(argv[1], &host, &path) == -1) {
+    proxy_host = proxy_user = proxy_password = host = path = NULL;
+    ind = 1;
+    if (!strcmp("-p", argv[ind])) {
+        if (!parse_proxy_param(argv[++ind], &proxy_host, &proxy_port, &proxy_user, &proxy_password)) {
+            fprintf(stderr, "Error - malformed proxy parameter '%s'.\n", argv[2]);
+            return 2;
+        }
+        ind++;
+    }
+
+    if (parse_url(argv[ind], &host, &path) == -1) {
         fprintf(stderr, "Error - malformed URL '%s'.\n", argv[1]);
         return 1;
     }
 
-    printf("Connecting to host '%s'", host);
+    if (proxy_host) {
+        printf("Connecting to host '%s'\n.", proxy_host);
+        host_name = gethostbyname(proxy_host);
+    } else {
+        printf("Connecting to host '%s'", host);
+        host_name = gethostbyname(host);
+    }
 
     client_connection = socket(PF_INET, SOCK_STREAM, 0);
-
     if (!client_connection) {
         fprintf(stderr, "Error, unable to create local socket.\n");
         return 2;
     }
-
-    host_name = gethostbyname(host);
 
     if (!host_name) {
         perror("Error in name resolution");
@@ -122,7 +197,7 @@ int main(int argc, char *argv[]) {
     }
 
     host_address.sin_family = AF_INET;
-    host_address.sin_port = htons(HTTP_PORT);
+    host_address.sin_port = htons(proxy_host ? proxy_port : HTTP_PORT);
     memcpy(&host_address.sin_addr, host_name->h_addr_list[0], sizeof(struct in_addr));
 
     if (connect(client_connection, (struct sockaddr *) &host_address, sizeof(host_address)) == -1) {
@@ -132,7 +207,7 @@ int main(int argc, char *argv[]) {
 
     printf("Retrieving document: '%s'\n", path);
 
-    http_get(client_connection, path, host);
+    http_get(client_connection, path, host, proxy_host, proxy_user, proxy_password);
 
     display_result(client_connection);
 
